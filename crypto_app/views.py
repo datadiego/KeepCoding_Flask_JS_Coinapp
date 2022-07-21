f3369761d82bd3b193dae861f7192637177547e4
Juan Diego <juandiegomariscal@gmail.com>
2022-07-21 21:47:30 +0200

    feat: 👍añadimos endpoints

--- crypto_app/views.py
+++ crypto_app/views.py
@@ -1,5 +1,21 @@
 from . import app
+from flask import render_template
 
-@app.route("/")
-def home():
+@app.route("/api/v1/movimientos")
+def movimientos():
     return "Hola flask"
+
+@app.route("/api/v1/tipo_cambio/<divisa_from>/<divisa_to>/<cantidad>")
+def tipo_cambio():
+    return "Valor unitario de la moneda destino en valor de origen"
+
+@app.route("/api/v1/movimiento")
+def alta_movimiento():
+    return "Grabará un nuevo movimiento"
+
+@app.route("/api/v1/staus")
+def estado_inversion():
+    return "Estado de la inversion"
+
+
+
