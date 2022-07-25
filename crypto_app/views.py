57058ef71b710084d50978fd755b9465d1f8b6a0
Juan Diego <juandiegomariscal@gmail.com>
2022-07-25 21:23:11 +0200

    feat: 👍el endpoint api/v1/movimiento inserta un movimiento predefinido

--- crypto_app/views.py
+++ crypto_app/views.py
@@ -2,7 +2,7 @@ from flask import jsonify
 import requests
 from . import app
 from crypto_app.models import DBManager
-from crypto_app.settings import APIKEY
+from crypto_app.settings import APIKEY, RUTA_DB
 #ENDPOINTS OBLIGATORIOS
 @app.route("/api/v1/movimientos")
 def movimientos():
@@ -21,13 +21,31 @@ def movimientos():
         print(error)
         return error
 
-@app.route("/api/v1/tipo_cambio/<divisa_from>/<divisa_to>/<cantidad>")
-def tipo_cambio():
-    return "Valor unitario de la moneda destino en valor de origen"
+@app.route("/api/v1/rate/<string:moneda_origen>/<string:moneda_destino>/<float:cantidad>")
+def rate(moneda_origen: str, moneda_destino: str, cantidad: float):
+    #TODO Comprobar si tenemos suficiente saldo para realizar la conversion
+    headers = {'X-CoinAPI-Key' : APIKEY}
+    url = f"https://rest.coinapi.io/v1/exchangerate/{moneda_origen}/{moneda_destino}"
+    respuesta = requests.get(url, headers=headers)
+    codigo = respuesta.status_code
+    data = respuesta.json()
+    price = {"tipo_cambio":data["rate"]*cantidad}
+    output = {"status":"success", "data":price}
+    return output
 
 @app.route("/api/v1/movimiento")
 def alta_movimiento():
-    return "Grabará un nuevo movimiento"
+    date = "2022-01-01"
+    time = "22:12"
+    moneda_from = "BTC"
+    cantidad_from = 5.0
+    moneda_to = "XTZ"
+    cantidad_to = 1000.0
+    sql = "INSERT INTO movimientos (date, time, moneda_from, cantidad_from, moneda_to, cantidad_to) VALUES ('2022-07-02','21:06', 'XTZ', 0.25, 'LTC', 5)"
+    db = DBManager(RUTA_DB)
+    estado = db.crear_movimiento(sql)
+    output = {"data":estado}
+    return output
 
 @app.route("/api/v1/staus")
 def estado_inversion():
@@ -50,15 +68,5 @@ def valor_monedas():
     output = {"status":"success", "data":assets}
     return output
 
-@app.route("/api/v1/rate/<string:moneda_origen>/<string:moneda_destino>/<float:cantidad>")
-def rate(moneda_origen: str, moneda_destino: str, cantidad: float):
-    #TODO Comprobar si tenemos suficiente saldo para realizar la conversion
-    headers = {'X-CoinAPI-Key' : APIKEY}
-    url = f"https://rest.coinapi.io/v1/exchangerate/{moneda_origen}/{moneda_destino}"
-    respuesta = requests.get(url, headers=headers)
-    codigo = respuesta.status_code
-    data = respuesta.json()
-    price = {"tipo_cambio":data["rate"]*cantidad}
-    output = {"status":"success", "data":price}
-    return output
+
 
