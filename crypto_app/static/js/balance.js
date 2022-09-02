f315786b31def1ff14a58016784bdd3363907ce8
Juan Diego <juandiegomariscal@gmail.com>
2022-09-02 18:55:14 +0200

    update: 🚀mejora la visualizacion del mensaje de "no hay movimientos"

--- crypto_app/static/js/balance.js
+++ crypto_app/static/js/balance.js
@@ -3,27 +3,22 @@ const peticion_rate = new XMLHttpRequest();
 const peticion_compra = new XMLHttpRequest();
 
 function obtenerMovimientos() {
-  console.log("####################")
-  console.log("Obteniendo movimientos");
   peticion_movimientos.open('GET', 'http://127.0.0.1:5000/api/v1/movimientos', true);
   peticion_movimientos.send();
-  console.log("FIN de obtener movimientos");
 }
 
 function mostrarMovimientos() {
-  console.log('entramos en mostrar movimientos');
   const tabla = document.querySelector('#cuerpo-tabla');
+  const mensajes = document.querySelector("#mensajes-control")
   let html = '';
   if (this.readyState === 4 && this.status === 200) {
-    console.log('--- TODO OK ----');
     const respuesta = JSON.parse(peticion_movimientos.responseText);
     const movimientos = respuesta.data;
-    
     if (movimientos.length === 0) {
-      tabla.innerHTML = '<tr><td colspan="5">No hay movimientos</td></tr>';
+      mensajes.innerHTML = 'No hay movimientos para mostrar';
     }
     
-    if(movimientos.length > 0){
+    if(movimientos.length >= 1){
     for (let i = 0; i < movimientos.length; i = i + 1) {
       const mov = movimientos[i];
       html = html + `
@@ -42,44 +37,31 @@ function mostrarMovimientos() {
   tabla.innerHTML = html; 
   } 
   else {
-    console.error('---- Algo ha ido mal en la petición ----');
     alert('Error al cargar los movimientos');
   }
-  console.log('FIN de mostrar movimientos');
 }
 
 function obtenerConversion() {
   //Probar a crear la peticion aqui dentro
-  console.log("######################")
-  console.log('entramos en obtener conversion');
   const moneda_from = document.querySelector('#moneda_from').value;
   const moneda_to = document.querySelector('#moneda_to').value;
   let cantidad = document.querySelector('#cantidad_from').value;
   cantidad = parseFloat(cantidad);
   cantidad = cantidad.toFixed(2);
-  console.log("moneda_from: " + moneda_from);
-  console.log("moneda_to: " + moneda_to);
-  console.log("cantidad: " + cantidad);
 
   peticion_rate.open('GET', `http://127.0.0.1:5000/api/v1/rate/${moneda_from}/${moneda_to}/${cantidad}`, false);
   peticion_rate.send();
   if(peticion_rate.status === 404){
     alert("No se pudo realizar la conversion, revisa los datos o intentalo mas tarde")
-
   }
   if (peticion_rate.readyState === 4 && peticion_rate.status === 200) {
-    console.log('--- TODO OK ----');
     const respuesta = JSON.parse(peticion_rate.responseText);
     const datos = respuesta.data;
-    console.log(datos);
     document.getElementById("conversion").value = datos.tipo_cambio.toFixed(2);
   }
-  
 }
 
 function compraMonedas() {
-  console.log("######################")
-  console.log('entramos en obtener conversion');
   const moneda_from = document.querySelector('#moneda_from').value;
   const moneda_to = document.querySelector('#moneda_to').value;
   let cantidad_from = parseFloat(document.querySelector('#cantidad_from').value);
@@ -87,11 +69,9 @@ function compraMonedas() {
   cantidad_from = parseFloat(cantidad_from);
   cantidad_from = cantidad_from.toFixed(2);
   
-
   peticion_rate.open('GET', `http://127.0.0.1:5000/api/v1/rate/${moneda_from}/${moneda_to}/${cantidad_from}`, false);
   peticion_rate.send();
   if (peticion_rate.readyState === 4 && peticion_rate.status === 200) {
-    console.log('--- TODO OK ----');
     const respuesta = JSON.parse(peticion_rate.responseText);
     const datos = respuesta.data;
     const cantidad_to = datos.tipo_cambio.toFixed(2);
@@ -99,14 +79,12 @@ function compraMonedas() {
     output = {"data": output_aux};
     peticion_compra.open('POST', 'http://127.0.0.1:5000/api/v1/movimiento', true);
     peticion_compra.setRequestHeader('Content-Type', 'application/json;charset=utf-8');
-    //console.log(data);
     peticion_compra.send(JSON.stringify(output_aux));
     peticion_compra.onload = location.reload()
   }
 }
 
 window.onload = function() {
-  console.log('Inicio de window.onload');
   obtenerMovimientos();
   peticion_movimientos.onload = mostrarMovimientos; //request de html
 
